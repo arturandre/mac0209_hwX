f70413c02968bd06b8a6ad87cc957cd6c8d97dc9
Artur Andre <arturpossible@gmail.com>
2021-03-09 18:12:14 -0300

    first commit

---
+++ ex2.py
@@ -0,0 +1,28 @@
+"""
+Usando o método de Euler preencha o código das funções
+abaixo para calcular:
+
+a partir da segunda derivada da posição (i.e. aceleração),
+primeiro a velocidade instantânea e com base na velocidade
+calcule a posição num dado instante "t".
+
+Equação da aceleração:
+
+    d2x/dt2 = c*t
+
+Onde c = 6
+"""
+
+def nextVeuler(v,t,dt):
+    pass
+    
+def nextXeuler(x,v,dt):
+    pass
+
+def velocidade(t, v0, c, dt):
+    pass
+
+def posicao(x0, v0, t, dt, n):
+    # x0 = 0
+    # v0 = 0
+    pass
