aa8db222290138ed336f74068bad7661c79f6d03
Artur Andre <arturpossible@gmail.com>
2021-03-09 18:26:45 -0300

    tests fixed

--- test_ex2.py
+++ test_ex2.py
@@ -2,12 +2,12 @@ from ex2 import velocidade, posicao
 
 eps = 1.19209e-07
 
-def test1_velocidade():
+def test_velocidade():
     assert velocidade(0, 0, 0.1, 1) == 0.0
     assert abs(velocidade(0, 0, 0.1, 2) - 0.06) < eps
     assert abs(velocidade(0, 0, 0.1, 20) - 62913.3) < eps
 
-def test2_posicao():
+def test_posicao():
     assert posicao(0, 0, 0, 0.1, 1) == 0.0
     assert abs(posicao(0, 0, 0, 0.1, 2) - 0.06) < eps
     assert abs(posicao(0, 0, 0, 0.1, 20) - 1069548.9) < eps
